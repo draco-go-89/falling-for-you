3ecbc74e6aaaf7d36b063b0262fc589291bdc313
Dhrubo Basumatary <lubolabao509@gmail.com>
2026-08-08 20:42:43 +0530

    added the drake music when user pointer over the page and baby, you my everything.............shiiiiiiiiiii

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 for (let i = 0; i < 90; i++) {
     const span = document.createElement('span');
-    span.innerHTML = "𝑓𝑎𝑙𝑙𝑖𝑛𝑔 𝑓ℴ𝑟 𝑦ℴ𝑢";
+    span.innerHTML = "baby, you my everything";
     span.className = 'falling-text';
     span.style.left = Math.random() * 90 + "vw";
     span.style.animationDuration = (Math.random() * 7 + 2) + "s";
     span.style.opacity = Math.random();
     document.body.appendChild(span);
 }
+
+const music = document.getElementById("bgMusic");
+
+document.addEventListener("click", () => {
+    music.play();
+}, { once: true });
