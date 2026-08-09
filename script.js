3979879e32b6c9353febb5137e4c316069168666
Dhrubo Basumatary <lubolabao509@gmail.com>
2026-08-09 13:55:12 +0530

    she;

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < 90; i++) {
     const span = document.createElement('span');
-    span.innerHTML = "hate that i made you love me";
+    span.innerHTML = "she;hate that i made you love me";
     span.className = 'falling-text';
     span.style.left = Math.random() * 90 + "vw";
     span.style.animationDuration = (Math.random() * 7 + 2) + "s";
