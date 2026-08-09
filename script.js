41a2c433cd0cedf2ea82c56d696064c50443731a
Dhrubo Basumatary <lubolabao509@gmail.com>
2026-08-09 10:11:59 +0530

    replaced the song drake with ariana grande and also changed the falling text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < 90; i++) {
     const span = document.createElement('span');
-    span.innerHTML = "baby, you my everything";
+    span.innerHTML = "hate that i made you love me";
     span.className = 'falling-text';
     span.style.left = Math.random() * 90 + "vw";
     span.style.animationDuration = (Math.random() * 7 + 2) + "s";
